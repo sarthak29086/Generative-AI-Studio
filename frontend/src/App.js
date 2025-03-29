7248e77e3a5436e3767fa8418983f960aa1c5a23
Shivam Gami <shivamprakashgami@gmail.com>
2025-03-29 11:40:45 +0530

    Recplaced old work with mine new work

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Tailwind CSS is Working! 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
